c0ba1baee9ec67e2a498be4c344852716137b7d9
Mai Lan Peng <mailan.peng@gmail.com>
2018-07-10 04:10:59 +0200

    final countdown

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,8 +1,1 @@
 #write your code here
-def translate(word)
-  if word.start_with?('a' || 'e' || 'i' || 'o' || 'u' || 'y')
-    word << 'ay'
-  else
-    word.gsub(word[0], '') << word[0] << 'ay'
-  end
-end
