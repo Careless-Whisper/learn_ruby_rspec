0af5ce2df3364af57440a1600c42ada96db97ede
Mai Lan Peng <mailan.peng@gmail.com>
2018-07-10 04:07:53 +0200

    final modifications

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -10,3 +10,16 @@ def repeat(str, n = 2)
   str2 = (str + ' ') * (n - 1) + str
   p str2
 end
+
+def start_of_word(string, letters)
+  return string[0, letters]
+end
+
+def first_word(string)
+  return string.split.first
+end
+
+def titleize(string)
+  small_words = %w{on the and}
+  return string.split.each_with_index.map{|word, index| small_words.include?(word) && index > 0 ? word : word.capitalize}.join(" ")
+end
